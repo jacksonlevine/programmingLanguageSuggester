9dc726eceed16b44935596b7e85311eac6515c8e
jacksonlevine <jack@netbug.us>
2023-01-13 09:37:51 -0800

    add more structure to get all form values

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,27 @@ onload = function() {
 
   form.onsubmit = function(event) {
     event.preventDefault();
+    let freeTimeOpts = document.getElementsByName("freeTime");
+    let weatherOpts = document.getElementsByName("weather");
 
     let favColor = document.getElementById("favColor").value;
     let favAnimal = document.getElementById("favAnimal").value;
+    let freeTime = "";
+    let weather = "";
+    for(let i of freeTimeOpts) {
+      if(i.checked) {
+        freeTime = i.id;
+      }
+    }
+    for(let i of weatherOpts) {
+      if(i.checked) {
+        weather = i.id;
+      }
+    }
+    let age = document.getElementById("age").value;
 
     let answerSpot = document.querySelector(".theAnswer");
+    console.log(favColor);
     answerSpot.innerHTML = "teststst";
   }
 }
